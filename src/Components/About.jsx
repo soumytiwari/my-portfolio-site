32346043f85092960d4d7d8fd5fd9db7de59b0d6
Soumya Tiwari <96687561+soumytiwari@users.noreply.github.com>
2024-12-25 07:26:29 +0000

    modified

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science and Engineering student studying at Indian Institute of Information Technology (IIIT), Kalyani. I enjoy working as a software engineer as well as a software developer.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "DSA",
+  "AWS",
+  "RDBMS",
+  "Prompt Engineering",
+  "Full Stack Development",
+  "Web Development",
+  "MERN Stack Development",
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. I'm a detail-driven technology enthusiast with a passion for solving complex problems. Leveraging my expertise in Data Structures and Algorithms, Relational Database Management Systems, and Amazon Web Services, I design and develop scalable, efficient, and secure solutions that drive innovation and growth.";
 
 const About = () => {
   return (
